49c95784986dfda42f5d7908f272886d8e72c281
stoolan <stoolan@telegeography.com>
2019-09-30 14:46:17 -0400

    updated script for image finder

--- plotly2js/scripts.py
+++ plotly2js/scripts.py
@@ -1,6 +1,6 @@
 def js_with_image(plot_div_id):
     return f"""
     var jsNode = document.getElementById('{plot_div_id}');
-        var img = document.getElementById("{plot_div_id}").parentElement.firstChild;
+        var img = document.getElementById("{plot_div_id}").parentElement.getElementsByTagName('img')[0];
         if(img && img.tagName == 'IMG') document.getElementById("{plot_div_id}").parentElement.removeChild(img);
     """
